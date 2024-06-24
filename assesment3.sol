e02442278a6d8ef046c10abe5f5fc9899eb39308
Sagarthakur18 <sagarthakur8456@gmail.com>
2024-06-24 22:10:35 +0530

    Update assesment3.sol

--- assesment3.sol
+++ assesment3.sol
@@ -1,1 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract CricketFanToken is ERC20, Ownable {
+    constructor() ERC20("CricketFanToken", "CFT") Ownable(msg.sender) {
+        // Mint initial supply to the contract deployer (owner)
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    // Function to retrieve the balance of an address
+    function balance(address account) public view returns (uint256) {
+        return balanceOf(account);
+    }
+}
 
